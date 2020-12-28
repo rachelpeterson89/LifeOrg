c314cb31898c31752cb415d371fffaac7d3901c3
Cameron Spano <spano.cameronwork@yahoo.com>
2020-12-28 11:55:03 -0600

    First Commit

---
+++ src/main/java/com/project/life/LifeOrgApplication.java
@@ -0,0 +1,13 @@
+package com.project.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeOrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/life/LifeOrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeOrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
